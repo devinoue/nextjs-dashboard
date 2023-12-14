ffe26d004228a86a28074f6abfc7b9ecc3352fa2
Inoue <itisagoodcat@gmail.com>
2023-12-15 02:54:48 +0900

    update

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,16 @@
 export default function Page() {
-  return <p>Customers Page</p>;
+  process.stdin.resume();
+  process.stdin.setEncoding("utf8");
+  // Your code here!
+  const list = [];
+
+  const domains = [];
+  for (const domain of list) {
+    if (domain.availability_id === null) {
+      console.log(domain.domain_name);
+      domains.push(domain.domain_name);
+    }
+  }
+
+  return <p>Customers Page {domains.join(" ")}</p>;
 }
